86074648f73cf196c1290f66f1dd9ff3b6df72e9
galgros <galgros@yahoo.fr>
2020-03-18 16:09:22 +0100

    include _footer.php on index, platform, about, contact

---
+++ _footer.php
@@ -0,0 +1,67 @@
+<footer>
+    <section class="items">
+        <article class="hidden">
+            <a href="plateformes.php">
+                <div>Platforms</div>
+            </a>
+        </article>
+        <article class="hidden">
+            <a href="produits.html">
+                <div>Products</div>
+            </a>
+        </article>
+        <article class="hidden">
+            <a href="about_us.php">
+                <div>About Us</div>
+            </a>
+        </article>
+        <article class="hidden">
+            <a href="index.php">
+                <div class="logo_retro_invaders">
+                    <img class="logo_desktop" alt="logo_retro_invaders_desktop" src="Images/Autres/blog-loco-green.png" height="150" width="auto">
+                </div>
+            </a>
+        </article>
+        <article class="hidden">
+            <a href="contact.php">
+                <div>Contact</div>
+            </a>
+        </article>
+        <article>
+            <div>FAQ</div>
+        </article>
+
+        <article>
+            <div>Legal Mentions</div>
+        </article>
+    </section>
+
+    <section class="logo-rs-footer-container">
+        <article>
+            <a href="https://www.instagram.com/?hl=fr">
+                <img class="logo_instagram" alt="logo_instagram" src="Images/Autres/instagram-green.png" height="40" width="auto">
+            </a>
+        </article>
+
+        <article>
+            <a href="https://twitter.com/?lang=fr">
+                <img alt="logo_twitter" src="Images/Autres/twitter-green.png" height="40" width="auto">
+            </a>
+        </article>
+
+        <article>
+            <a href="https://fr-fr.facebook.com/">
+                <img alt="logo_facebook" src="Images/Autres/facebook-green.png" height="40" width="auto">
+            </a>
+        </article>
+
+        <article>
+            <a href="https://www.pinterest.fr/">
+                <img alt="logo_pinterest" src="Images/Autres/pinterest-green.png" height="40" width="auto">
+            </a>
+        </article>
+
+    </section>
+</footer>
+
+
